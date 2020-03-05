499af5a3bcc8aee6fcb86cd4eff4741f789dabaf
Lorena Florescu <lorelaywilson@gmail.com>
2020-03-05 01:06:13 +0100

    gitflow-feature-stash: post-sesion

--- src/api/v1.0/modelos/post-sesion.php
+++ src/api/v1.0/modelos/post-sesion.php
@@ -6,8 +6,9 @@ if(isset($_POST['key']) && $_POST['key'] != '' && $_POST['user'] && $_POST['user
     $user = $_POST['user'];
 
     $name = "SELECT `name, role` FROM `user` WHERE `name` = $user AND `password` = $key";
+    $res = mysqli_query($conection, $name);
 
-    if($res = mysqli_query($conection, $name)) {
+    if($res && mysqli_num_rows($res)==1) {
 
             $row = mysqli_fetch_assoc($res);
             session_start();
