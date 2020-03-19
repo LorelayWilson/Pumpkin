6f694494ef739769252474e0469a235bd3f616db
Lorena Florescu <lorelaywilson@gmail.com>
2020-03-19 18:29:36 +0100

    hfdf

--- src/api/includes/conection.php
+++
@@ -1,9 +0,0 @@
-<?php
-$bbdd_server = 'localhost';
-$bbdd_user = 'liflores_Lorelay';
-$bbdd_password ='Asdf1234*';
-$bbdd = 'liflores_Pumpkin';
-
-$conection = mysqli_connect($bbdd_server, $bbdd_user, $bbdd_password, $bbdd);
-
-mysqli_query($conection, 'SET NAME utf8');
