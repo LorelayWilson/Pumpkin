008179ba217164529c17cbd62c812b978f34312f
Lorena Florescu <lorelaywilson@gmail.com>
2020-03-20 00:37:47 +0100

    backend

--- src/api/v1.0/modelos/post-sesion.php
+++ src/api/v1.0/modelos/post-sesion.php
@@ -1,32 +1,66 @@
 <?php
+ //
 
-if(isset($_POST['key']) && $_POST['key'] != '' && $_POST['user'] && $_POST['user'] != ''){
+if (isset($_POST['password']) && $_POST['password'] != '' && isset($_POST['user']) && $_POST['user'] != '') {
 
-    $key = $_POST['key'];
     $user = $_POST['user'];
+$password = $_POST['password'];
+$conexion = mysqli_connect("localhost", "root", "", "bbdd_pumpkin");
+$consulta ="SELECT  user.id as userId,
+user.name as userName,
+user.password as userPassword,
+user.rol as userRol,
+role.*  FROM `user`, `role` WHERE user.name='$user' and user.password='$password' and user.rol = role.id ";
+$resultado=mysqli_query($conexion, $consulta);
+//------------------
+//$mostrar=mysqli_fetch_array($resultado)
+    
+//--------------------------------
+    
+filas = mysqli_num_rows($resultado);
+    /*if($filas>0)*/
 
-    $name = "SELECT `name, role` FROM `user` WHERE `name` = $user AND `password` = $key";
-    $res = mysqli_query($conection, $name);
-
-    if($res && mysqli_num_rows($res)==1) {
-
-            $row = mysqli_fetch_assoc($res);
-            session_start();
-            $_SESSION['registrado'] = 'ok';
-            $_SESSION['name'] = $row["name"];
-            $_SESSION['role'] = $row["role"];
-            $http_code = 200;
-
-    } else{
-
-        //error de autentificación
-        $http_code = 401;
-        die('{"error" : "no autorizado"}');
-    }
-
-} else {
-
-    //petición mal formada. Error del cliente.
-    $http_code = 400;
-    die('{"error" : "faltan parámetros"}');
+if($filas>0){
+    session_start();
+        $_SESSION['registered'] = 'ok';
+        $http_code = 200;
+    
+} else{
+    $http_code = 401;
+    echo "No existe el usuario";
+    
 }
+}
+else {
+    $http_code = 400;
+    echo "Falta el usuario o la contraseña";
+}
+/*
+if (isset($_POST['contraseña']) && $_POST['contraseña'] != '' && isset($_POST['nombre_usuario']) && $_POST['nombre_usuario'] != '') {
+    $user = $_POST['nombre_usuario'];
+$password = $_POST['contraseña'];
+$conexion = mysqli_connect("localhost", "root", "", "bbdd_pumpkin_parcelas");
+$consulta ="SELECT  usuarios.id_usuario as usuarioId,
+usuarios.nombre_usuario as usuarioNombre,
+usuarios.contraseña as usuarioContraseña,
+usuarios.id_rol as usuarioRol,
+roles.*  FROM `usuarios`, `roles` WHERE usuario.nombre_usuario='$user' and usuarios.contraseña='$password' and usuarios.id_rol = roles.id_rol ";
+$resultado=mysqli_query($conexion, $consulta);
+
+$filas = mysqli_num_rows($resultado);
+
+if($filas>0){
+    session_start();
+        $_SESSION['registered'] = 'ok';
+        $http_code = 200;
+    
+} else{
+   
+    $http_code = 401;
+    echo "No existe el usuario";
+    
+}
+}
+else {
+    $http_code = 400;
+}*/
