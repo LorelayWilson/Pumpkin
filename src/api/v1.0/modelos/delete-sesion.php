8aa706a86b62ede063e1cb92870ee0c8a9ca8aaa
Denis <denis-fg@hotmail.com>
2020-04-22 17:31:41 +0200

    update

--- src/api/v1.0/modelos/delete-sesion.php
+++ src/api/v1.0/modelos/delete-sesion.php
@@ -4,14 +4,14 @@ session_start(); //Iniciamos la sesión
 unset($_SESSION['registered']); //Eliminamos el parámetro 'registrado' -->
 session_destroy(); //Destruimos la sesión -->
 
-$http_code = 200; //Devolvemos código 200
-/*
+$http_code = 200;//Devolvemos código 200
+
 if (isset($_SESSION['registered']) && $_SESSION['registered'] == 'ok') {
     unset($_SESSION['registered']); //Eliminamos el parámetro 'registrado' -->
     session_destroy(); //Destruimos la sesión -->
     $http_code = 200;
     
-}*/
+}
 else {
     $http_code = 404;
 }
